c75ed2dacbff6d92a25f05b34fb7509d83d3c988
HectorJS <49287597+HectorJS@users.noreply.github.com>
2021-05-10 00:34:09 -0500

    FIXED CAMERA

--- TestGLProj/Camera.cpp
+++ TestGLProj/Camera.cpp
@@ -42,11 +42,11 @@ glm::mat4 Camera::SetViewMatrix(Car car)
 		speedMod * car._3pDistance * 0.25f,													// y camera position
 		speedMod * car._3pDistance * cosf(car.toRad(car.modelRotAngle + smoothCamTurn)));	// z camera position
 
-	if (!flyCamMode) /// Fly camera
+	if (flyCamMode) /// Fly camera
 	{
-		//camPosition = glm::translate(camX, camY, camZ);
-		camPosition = glm::translate(0.f, -180.f, -0.f);
-		//view = glm::rotate((float)verCam, 1.0f, 0.0f, 0.0f);	// Vertical camera movement - PITCH
+		camPosition = glm::translate(camX, camY, camZ);
+		//camPosition = glm::translate(0.f, -180.f, -0.f);
+		view = glm::rotate((float)verCam, 1.0f, 0.0f, 0.0f);	// Vertical camera movement - PITCH
 		view = glm::rotate(-90.0f, -1.0f, 0.0f, 0.0f);
 		view *= glm::rotate((float)horCam, 0.0f, 1.0f, 0.0f);	// Horizontal camera movement - YAW
 		view *= camPosition;									// Camera position
